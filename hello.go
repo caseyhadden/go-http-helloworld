7d979009e06d71c956b64095818ed27600d9ffc8
Casey Hadden <me@caseyhadden.com>
2015-05-11 21:30:03 -0700

    Include argument for port value

--- hello.go
+++ hello.go
@@ -3,6 +3,7 @@ package main
 import (
     "net/http" //package for http based web programs
     "fmt"
+    "os"
 )
 
 func handler(w http.ResponseWriter, r *http.Request) { 
@@ -14,6 +15,10 @@ func main() {
     fmt.Println("Initializing Hello World...")
     http.HandleFunc("/", handler) // redirect all urls to the handler function
     fmt.Println("DONE setting handler...")
-    http.ListenAndServe(":8080", nil) // listen for connections at port 9999 on the local machine
+    port := "8080" // default to port 8080
+    if len(os.Args) > 1 {
+        port = os.Args[1] // command line argument can be provided for port
+    }
+    http.ListenAndServe(":" + port, nil) // listen for connections at port on the local machine
     fmt.Println("DONE Initializing Hello World...")
 }
